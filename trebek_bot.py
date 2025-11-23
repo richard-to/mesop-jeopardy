154254d1ecfd5732709f6501f7d10d83d12549c3
Richard <gohn76@gmail.com>
2025-11-22 16:49:22 -0800

    Update mesop version and Gemini model version

--- trebek_bot.py
+++ trebek_bot.py
@@ -4,18 +4,21 @@ import os
 import google.generativeai as genai
 
 
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
-answer_check_model = genai.GenerativeModel('gemini-1.5-pro')
-question_gen_model = genai.GenerativeModel('gemini-1.5-flash', generation_config={
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 64,
-  "max_output_tokens": 16384,
-  "response_mime_type": "application/json",
-})
+answer_check_model = genai.GenerativeModel("gemini-flash-latest")
+question_gen_model = genai.GenerativeModel(
+    "gemini-flash-latest",
+    generation_config={
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 64,
+        "max_output_tokens": 16384,
+        "response_mime_type": "application/json",
+    },
+)
 
 
 _JEOPARDY_QUESTION_GENERATE_PROMPT = """
@@ -70,45 +73,45 @@ Afterwards, elaborate on why.
 
 
 def check_answer(clue: str, answer: str, response: str) -> list[bool, str]:
-  """Checks if the given answer is correct.
+    """Checks if the given answer is correct.
 
-  Args:
-    clue: Clue being presented
-    answer: The real response to the clue
-    response: The user's response to the clue
+    Args:
+      clue: Clue being presented
+      answer: The real response to the clue
+      response: The user's response to the clue
 
-  Returns:
-    bool: Whether the response to the clue was correct
-    answer_response: Explanation on why the user's response was right/wrong
-  """
-  response = answer_check_model.generate_content(_JEOPARDY_PROMPT.format(
-    clue=clue, question=answer, response=response)
-  ).text
+    Returns:
+      bool: Whether the response to the clue was correct
+      answer_response: Explanation on why the user's response was right/wrong
+    """
+    response = answer_check_model.generate_content(
+        _JEOPARDY_PROMPT.format(clue=clue, question=answer, response=response)
+    ).text
 
-  if response.startswith("Yes. That is correct."):
-    return True, response[len("Yes. That is correct."):]
-  return False, response[len("No. That is incorrect."):]
+    if response.startswith("Yes. That is correct."):
+        return True, response[len("Yes. That is correct.") :]
+    return False, response[len("No. That is incorrect.") :]
 
 
 def generate_questions() -> list[dict[str, str]]:
-  """Generate Jeopardy questions using Gemini.
+    """Generate Jeopardy questions using Gemini.
 
-  Returns:
-    Generated jeopardy data set in the expected format.
-  """
-  question_sets = json.loads(
-    question_gen_model.generate_content(_JEOPARDY_QUESTION_GENERATE_PROMPT).text
-  )
-  questions_list = []
-  # Format the questions like the data set.
-  for category, questions in question_sets.items():
-    for question in questions:
-      questions_list.append(
-        {
-          "category": category,
-          "air_date": "2024-1-1",
-          "show_number": "0",
-          **question
-        }
-      )
-  return questions_list
+    Returns:
+      Generated jeopardy data set in the expected format.
+    """
+    question_sets = json.loads(
+        question_gen_model.generate_content(_JEOPARDY_QUESTION_GENERATE_PROMPT).text
+    )
+    questions_list = []
+    # Format the questions like the data set.
+    for category, questions in question_sets.items():
+        for question in questions:
+            questions_list.append(
+                {
+                    "category": category,
+                    "air_date": "2024-1-1",
+                    "show_number": "0",
+                    **question,
+                }
+            )
+    return questions_list
